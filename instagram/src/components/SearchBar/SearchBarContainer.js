c702b37e6e829bf2e175b34498e49687cc9e09e8
Michael Jenkins <mjenkins9605@vidangel.com>
2019-03-13 19:34:27 -0400

    started search bar

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -1,4 +1,20 @@
 import React from "react";
 
+import SearchBar from "./SearchBar.css";
+import instagramBanner from "./instagramBanner";
+
+function SearchBarContainer (props) {
+    return (
+        <div>
+            <p><i class="fab fa-instagram"></i></p>
+            <p>|</p>
+            <instagramBanner />
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    );
+}
 
 export default SearchBarContainer;
