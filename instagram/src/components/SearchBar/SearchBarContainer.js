a1ed0fb458033d7bfa462336c62a6e2d2b22515f
Michael Jenkins <mjenkins9605@vidangel.com>
2019-03-13 21:09:37 -0400

    img

--- instagram/src/components/SearchBar/SearchBarContainer.js
+++ instagram/src/components/SearchBar/SearchBarContainer.js
@@ -5,14 +5,17 @@ import instagramBanner from "./instagramBanner";
 
 function SearchBarContainer (props) {
     return (
-        <div>
+        <div className="searchBar">
             <p><i class="fab fa-instagram"></i></p>
             <p>|</p>
             <instagramBanner />
-            <p></p>
-            <p></p>
-            <p></p>
-            <p></p>
+
+            <input type="text" value="Search" />
+            <div className="rightIcons">
+                <p><i class="far fa-compass"></i></p>
+                <p><i class="far fa-heart"></i></p>
+                <p><i class="far fa-user"></i></p>
+            </div>
         </div>
     );
 }
